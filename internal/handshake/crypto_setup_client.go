0789d7d59141d03c2f69db113a43fbe0f28f37fb
Abdoueck632 <abdouseck632@gmail.com>
2023-01-25 13:39:45 +0100

    Setter fo obit and ID for relay

--- internal/handshake/crypto_setup_client.go
+++ internal/handshake/crypto_setup_client.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"os"
 	"sync"
 	"time"
@@ -670,6 +671,11 @@ func (h *cryptoSetupClient) SetDerivationKey(otherKey []byte, myKey []byte, othe
 func (h *cryptoSetupClient) GetOncesObitID() ([]byte, []byte, []byte) {
 	return h.diversificationNonce, nil, nil
 }
-func (h *cryptoSetupClient) SetOnces(diversifi []byte) {
+func (h *cryptoSetupClient) SetOncesObitID(diversifi []byte, obit []byte, ID []byte) {
 	h.diversificationNonce = diversifi
+}
+
+// Normally I have to convert net.addr to string for implementation in client side. But I'll skip it this time and just focus on the server part.
+func (h *cryptoSetupClient) SetRemoteAddr(addr net.Addr) {
+
 }
--- internal/handshake/interface.go
+++ internal/handshake/interface.go
@@ -1,6 +1,10 @@
 package handshake
 
-import "github.com/Abdoueck632/mp-quic/internal/protocol"
+import (
+	"net"
+
+	"github.com/Abdoueck632/mp-quic/internal/protocol"
+)
 
 // Sealer seals a packet
 type Sealer interface {
@@ -21,7 +25,8 @@ type CryptoSetup interface {
 	GetSealerForCryptoStream() (protocol.EncryptionLevel, Sealer)
 	SetDerivationKey(otherKey []byte, myKey []byte, otherIV []byte, myIV []byte)
 	GetOncesObitID() ([]byte, []byte, []byte)
-	SetOnces(diversifi []byte)
+	SetOncesObitID(diversifi []byte, obit []byte, ID []byte)
+	SetRemoteAddr(addr net.Addr)
 }
 
 // TransportParameters are parameters sent to the peer during the handshake
