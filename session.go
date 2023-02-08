e49d2154c9976a8642faa8ab18b0b7438223c03e
Abdoueck632 <abdouseck632@gmail.com>
2023-02-08 15:41:29 +0100

    delete the len of getpathsAndLen

--- session.go
+++ session.go
@@ -987,6 +987,6 @@ func (s *session) SetDerivateKey(otherKey []byte, myKey []byte, otherIV []byte, 
 func (s *session) GetCryptoSetup() handshake.CryptoSetup {
 	return s.cryptoSetup
 }
-func (s *session) GetpathsAndLen() (int, *path) {
-	return len(s.paths), s.paths[0]
+func (s *session) GetpathsAndLen() *path {
+	return s.paths[0]
 }
