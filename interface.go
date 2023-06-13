df026a9dcbdf08efa3de057e571ab9c99633f68d
Abdoueck632 <abdouseck632@gmail.com>
2023-06-13 15:38:22 +0200

    ajout de la fonction pour l'incrementation du fenetre d'envoi

--- interface.go
+++ interface.go
@@ -59,6 +59,7 @@ type Stream interface {
 	SetReadPosInFrame(readPosInFrame int)
 	SetReadOffset(readOffset uint64)
 	Setuint64(writeOffset uint64)
+	IncrementHighestReceived(increment protocol.ByteCount)
 }
 
 // A Session is a QUIC connection between two peers.
--- stream.go
+++ stream.go
@@ -451,3 +451,6 @@ func (s *stream) SetReadOffset(readOffset uint64) {
 func (s *stream) Setuint64(writeOffset uint64) {
 	s.writeOffset = protocol.ByteCount(writeOffset)
 }
+func (s *stream) IncrementHighestReceived(increment protocol.ByteCount) {
+	s.writeOffset = protocol.ByteCount(increment)
+}
