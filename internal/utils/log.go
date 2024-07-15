b51a7d5dc656e6acb90c98e734aa79a5c39e7716
Abdoueck632 <abdouseck632@gmail.com>
2024-07-15 09:56:12 +0200

    affichage deslogs

--- internal/utils/log.go
+++ internal/utils/log.go
@@ -19,9 +19,9 @@ const (
 	LogLevelNothing LogLevel = iota
 
 	// LogLevelError enables err logs
-	LogLevelError //= LogLevelNothing
+	LogLevelError = LogLevelNothing
 	// LogLevelInfo enables info logs (e.g. packets)
-	LogLevelInfo //= LogLevelNothing
+	LogLevelInfo = LogLevelNothing
 	// LogLevelDebug enables debug logs (e.g. packet contents)
 	LogLevelDebug //= LogLevelNothing
 )
